adc414a1f699fc5c73936b443dc6ae77329e70a7
AifeDunne <aifedunne@gmail.com>
2017-08-03 12:10:59 -0600

    Add Files

---
+++ sideFunctions.php
@@ -0,0 +1,15 @@
+<?php
+if (isset($_POST['checkUsers'])) {
+$path = $_SERVER['DOCUMENT_ROOT'];
+include_once $path . '/wp-config.php';
+include_once $path . '/wp-load.php';
+include_once $path . '/wp-includes/pluggable.php';
+$error = "none";
+$userName =  $_POST['checkUsers'];
+$uEmail = $_POST['checkEmail'];
+if ( username_exists( $userName ) ) { $error = 'un-exists'; }
+else if ( ! validate_username( $userName ) ) { $error = 'un-invalid'; }
+if ( !is_email( $uEmail ) ) { $error = 'email-invalid'; }
+else if ( email_exists( $uEmail ) ) { $error = 'email-exists'; }
+echo $error; }
+?>
