db03392139aa565379958ea12d223b62bedf083c
Aife Dunne <aifedunne@gmail.com>
2017-08-03 12:20:36 -0600

    Spacing

--- sideFunctions.php
+++ sideFunctions.php
@@ -1,9 +1,10 @@
 <?php
-if (isset($_POST['checkUsers'])) {
+if (isset($_POST['checkUsers'])) { 
 $path = $_SERVER['DOCUMENT_ROOT'];
 include_once $path . '/wp-config.php';
 include_once $path . '/wp-load.php';
-include_once $path . '/wp-includes/pluggable.php';
+include_once $path . '/wp-includes/pluggable.php';
+
 $error = "none";
 $userName =  $_POST['checkUsers'];
 $uEmail = $_POST['checkEmail'];
@@ -11,5 +12,5 @@ if ( username_exists( $userName ) ) { $error = 'un-exists'; }
 else if ( ! validate_username( $userName ) ) { $error = 'un-invalid'; }
 if ( !is_email( $uEmail ) ) { $error = 'email-invalid'; }
 else if ( email_exists( $uEmail ) ) { $error = 'email-exists'; }
-echo $error; }
+echo $error; }
 ?>
